1e0f5e7987f60e1cfa2080b428ac4c398cb8883d
nurhan004 <sharshenbekov777@gmail.com>
2024-10-24 16:29:05 +0600

    первый коммит

---
+++ app.js
@@ -0,0 +1,44 @@
+//dom
+// const title = window.document.getElementsByTagName("h1")
+// console.log(title);
+
+// const box  = document.getElementsByClassName("box")
+// console.log(box);
+
+// const id = document.getElementById("#id")
+// console.log(id);
+
+// const paragraf = document.querySelector("p")
+// console.log(paragraf);
+
+
+
+
+
+// document.getElementById('myButton').onclick = () => {
+//     console.log('Кнопка работает');
+// };
+
+
+//homework
+// document.getElementById("myInput").onchange("input", function() {
+//     var inputText = this.value;
+//     document.getElementById("myHeading").textContent = inputText;
+// });
+
+
+
+//2
+document.getElementById("Input").onchange = function() {
+    document.getElementById("h").textContent = this.value;
+};
+
+
+
+
+
+
+
+
+
+
